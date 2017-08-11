0ee4d8dcbac38f152e3b3c661e969de4fc8d51d2
arthur <arthur.dailland@etu.univ-nantes.fr>
2017-08-11 12:00:16 -0400

    add jar

--- src/dpcomment/Dpcomment.java
+++ src/dpcomment/Dpcomment.java
@@ -6,9 +6,6 @@ import cross.feature.*;
 public class Dpcomment {
 	
 	public static void main(String[] args) throws Exception {
-		/*System.out.println("*** GIOVANNI'S CODE ***");
-		PyhtonScriptInterpreter interpreter = new PyhtonScriptInterpreter();
-		interpreter.executeScript("techpriest-master/techpriest.py");*/
 		System.out.println("*** GENTLEBOT ***");
 		Gentlebot gb = new Gentlebot();
 		System.out.println("\n*** STEP 1 ***");
